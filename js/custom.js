a1a0808199a8adfcb123a80f6078e89f28921650
Rokon <rokonuaman@digitalfireflymarketing.com>
2023-11-18 00:41:43 +0600

    add-changes

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,24 @@
-(function($) {
+(function(jQuery) {
 
     "use strict";
 	
-    $(document).ready(function() {
+    jQuery(document).ready(function() {
 		
 		// PRELOADER
-        $("body").toggleClass("loaded");
+        jQuery("body").toggleClass("loaded");
         setTimeout(function() {
-            $("body").addClass("loaded");
+            jQuery("body").addClass("loaded");
         }, 3000);
 		
 		// PORTFOLIO DIRECTION AWARE HOVER EFFECT
-		var item = $("#bl-work-items>div");
+		var item = jQuery("#bl-work-items>div");
 		var elementsLength = item.length;
 		for (var i = 0; i < elementsLength; i++) {
-			$(item[i]).hoverdir();
+			jQuery(item[i]).hoverdir();
 		}
 		
 		// TEXT ROTATOR
-		$("#selector").animatedHeadline({
+		jQuery("#selector").animatedHeadline({
              animationType: "clip"
         });
 		
@@ -26,26 +26,26 @@
         Boxlayout.init();
 		
 		// REMOVE # FROM URL
-		$("a[href='#']").on("click", (function(e) {
+		jQuery("a[href='#']").on("click", (function(e) {
 			e.preventDefault();
 		}));
 
         // AJAX CONTACT FORM
-        $(".contactform").on("submit", function() {
-            $(".output_message").text("Loading...");
+        jQuery(".contactform").on("submit", function() {
+            jQuery(".output_message").text("Loading...");
 
-            var form = $(this);
-            $.ajax({
+            var form = jQuery(this);
+            jQuery.ajax({
                 url: form.attr("action"),
                 method: form.attr("method"),
                 data: form.serialize(),
                 success: function(result) {
                     if (result == "success") {
-                        $(".contactform").find(".output_message").addClass("success");
-                        $(".output_message").text("Message Sent!");
+                        jQuery(".contactform").find(".output_message").addClass("success");
+                        jQuery(".output_message").text("Message Sent!");
                     } else {
-                        $(".contactform").find(".output_message").addClass("error");
-                        $(".output_message").text("Error Sending!");
+                        jQuery(".contactform").find(".output_message").addClass("error");
+                        jQuery(".output_message").text("Error Sending!");
                     }
                 }
             });
@@ -54,21 +54,21 @@
         });
 
 		// MATERIAL CAROUSEL
-        $(".carousel.carousel-slider").carousel({
+        jQuery(".carousel.carousel-slider").carousel({
             fullWidth: true,
             indicators: true,
         });
 		
 		// RESUME CARDS ANIMATION
-		if ($(window).width() > 800) {
-			$(".resume-list-item, .resume-card").on("click", function() {
-				$(".resume-list-item").removeClass("is-active");
-				var e = parseInt($(this).data("index"),10);
-				$("#resume-list-item-" + e).addClass("is-active");
+		if (jQuery(window).width() > 800) {
+			jQuery(".resume-list-item, .resume-card").on("click", function() {
+				jQuery(".resume-list-item").removeClass("is-active");
+				var e = parseInt(jQuery(this).data("index"),10);
+				jQuery("#resume-list-item-" + e).addClass("is-active");
 				var t = e + 1,
 					n = e - 1,
 					i = e - 2;
-				$(".resume-card").removeClass("front back up-front up-up-front back-back"), $(".resume-card-" + e).addClass("front"), $(".resume-card-" + t).addClass("back"), $(".resume-card-" + n).addClass("back-back"), $(".resume-card-" + i).addClass("back")
+				jQuery(".resume-card").removeClass("front back up-front up-up-front back-back"), jQuery(".resume-card-" + e).addClass("front"), jQuery(".resume-card-" + t).addClass("back"), jQuery(".resume-card-" + n).addClass("back-back"), jQuery(".resume-card-" + i).addClass("back")
 			});
 		}
 		
